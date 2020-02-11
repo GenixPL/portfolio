68215e95e86e20be25ddd7458b8dea2407d0d792
genix <intergenixyt@gmail.com>
2020-02-11 12:03:58 +0100

    Clean and add basics.

---
+++ lib/navigation/router.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/navigation/routes.dart';
+import 'package:portfolio/ui/home/home.dart';
+import 'package:portfolio/utils/logger.dart';
+import 'package:portfolio/navigation/stack.dart' as s;
+
+class Router with NavigatorObserver {
+  static const String _TAG = 'RouteGenerator';
+
+  s.Stack<String> _routes = s.Stack();
+
+  get lastRoute => _routes.top();
+
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    _routes.push(settings.name);
+    Log.d(_TAG, 'generateRoute (${settings.name})');
+
+    switch (settings.name) {
+      case homeRoute:
+        return MaterialPageRoute(builder: (_) => HomePage());
+
+      default:
+        return _errorRoute('No path specified for: ${settings.name}.');
+    }
+  }
+
+  Route<dynamic> _errorRoute(String msg) {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        body: Center(
+          child: Text('ERROR ROUTE \n$msg'),
+        ),
+      );
+    });
+  }
+
+  @override
+  void didPop(Route route, Route previousRoute) {
+    if (route is MaterialPageRoute) {
+      _routes.pop();
+      Log.d(_TAG, 'popped page');
+    } else {
+      Log.d(_TAG, 'popped not page');
+    }
+
+    super.didPop(route, previousRoute);
+  }
+
+  // Navigator.pushNamedAndRemoveUntil should be used only with
+  // homeRoute being pushed.
+  @override
+  void didRemove(Route route, Route previousRoute) {
+    var top = lastRoute;
+    if (top == homeRoute) {
+      _routes = s.Stack();
+      _routes.push(homeRoute);
+    } else {
+      _routes = s.Stack();
+      _routes.push(homeRoute);
+      _routes.push(top);
+    }
+
+    super.didRemove(route, previousRoute);
+  }
+}
